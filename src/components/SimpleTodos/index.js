9df6607b9245be7d4e6c68824a6bfd6c122511ce
preethi-pathiwada <preethipathiwada@gmail.com>
2024-10-03 11:54:21 +0000

    added funtionality

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -40,7 +40,11 @@ const initialTodosList = [
 // Write your code here
 
 class SimpleTodos extends Component {
-  state = {todoList: initialTodosList}
+  state = {todoList: initialTodosList, inputValue: ''}
+
+  onChangeInput = event => {
+    this.setState({inputValue: event.target.value})
+  }
 
   deleteTodo = todoId => {
     console.log(todoId)
@@ -49,21 +53,46 @@ class SimpleTodos extends Component {
     this.setState({todoList: filteredTodoList})
   }
 
+  onAddTodo = () => {
+    const {inputValue, todoList} = this.state
+    this.setState(prevState => ({
+      todoList: [
+        ...prevState.todoList,
+        {id: todoList.length + 1, title: inputValue},
+      ],
+      inputValue: '',
+    }))
+  }
+
   render() {
-    const {todoList} = this.state
+    const {todoList, inputValue} = this.state
     return (
       <div className="bg">
         <div className="card">
           <h1 className="heading">Simple Todos</h1>
-          <ul className="todos-list-container">
-            {todoList.map(eachTodo => (
-              <TodoItem
-                key={eachTodo.id}
-                todoDetails={eachTodo}
-                deleteTodo={this.deleteTodo}
-              />
-            ))}
-          </ul>
+          <div className="add-todo-con">
+            <input
+              type="text"
+              placeholder="Create a Todo"
+              value={inputValue}
+              className="create-todo-input"
+              onChange={this.onChangeInput}
+            />
+            <button className="add-btn" onClick={this.onAddTodo}>
+              Add
+            </button>
+          </div>
+          <div className="list-con">
+            <ul className="todos-list-container">
+              {todoList.map(eachTodo => (
+                <TodoItem
+                  key={eachTodo.id}
+                  todoDetails={eachTodo}
+                  deleteTodo={this.deleteTodo}
+                />
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     )
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -4,14 +4,64 @@ import './index.css'
 import {Component} from 'react'
 
 class TodoItem extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      edit: true,
+      updatedTitle: props.todoDetails.title,
+      isChecked: false,
+    }
+  }
+
+  editOrSave = () => {
+    this.setState(prevState => ({edit: !prevState.edit}))
+  }
+
+  onChangeTitle = event => {
+    this.setState({updatedTitle: event.target.value})
+  }
+
+  onChangeCheckbox = event => {
+    this.setState(prevState => ({isChecked: !prevState.isChecked}))
+  }
+
   render() {
     const {todoDetails, deleteTodo} = this.props
     const {id, title} = todoDetails
+    const {edit, updatedTitle, isChecked} = this.state
+    console.log(updatedTitle)
 
     return (
       <li className="todo-item">
-        <p className="todo">{title}</p>
-        <button type="button" onClick={() => deleteTodo(id)}>
+        <div className="title-and-edit-con">
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={this.onChangeCheckbox}
+          />
+          {edit ? (
+            <p className={isChecked ? 'todo text' : 'todo'}>{updatedTitle}</p>
+          ) : (
+            <input
+              type="input"
+              className="edit-input"
+              onChange={this.onChangeTitle}
+              value={updatedTitle}
+            />
+          )}
+          {isChecked ? (
+            ''
+          ) : (
+            <button className="edit-btn" onClick={this.editOrSave}>
+              {edit ? 'Edit' : 'Save'}
+            </button>
+          )}
+        </div>
+        <button
+          type="button"
+          className="del-btn"
+          onClick={() => deleteTodo(id)}
+        >
           Delete
         </button>
       </li>
